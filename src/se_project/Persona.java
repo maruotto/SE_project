614ad6261314b76df9fe06fa2a2e6b2ae3037223
luigi <luigi@air-di-luigi.home>
2021-11-17 21:44:05 +0100

    Person class

---
+++ src/se_project/Persona.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package se_project;
+
+/**
+ *
+ * @author luigi
+ */
+public class Persona {
+    
+    private String name;
+    private String surname;
+
+    public Persona(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    
+    
+    
+}
