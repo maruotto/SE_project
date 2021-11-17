f05a3f99df63e821d4f67b774734b248c056edff
maruotto <94022256+maruotto@users.noreply.github.com>
2021-11-17 21:47:01 +0100

    merge complicato

--- src/se_project/SE_Project.java
+++ src/se_project/SE_Project.java
@@ -17,6 +17,7 @@ public class SE_Project {
         // TODO code application logic here
         System.out.println("hello giggi");
         System.out.println("Hello Gianluca");
+        System.out.println("merge complicata");
         System.out.println("hello ida");
         
         System.out.println("Si è sminkiato ubuntu");
