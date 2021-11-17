2c18770252050cfa07440d1d4e2644bf54955cc0
maruotto <94022256+maruotto@users.noreply.github.com>
2021-11-17 20:54:22 +0100

    giggi added

--- src/se_project/SE_Project.java
+++ src/se_project/SE_Project.java
@@ -15,6 +15,7 @@ public class SE_Project {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello giggi");
     }
     
 }
