78b38cca0f6004e53b2cb1dd520d1aab1e43097c
maruotto <94022256+maruotto@users.noreply.github.com>
2021-11-23 11:56:21 +0100

    create class NumberStack

---
+++ src/se_project_g9/NumberStack.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package se_project_g9;
+import java.util.Stack;
+
+/**
+ *
+ * @author gruppo9
+ */
+public class NumberStack<E> extends Stack{
+    
+}
