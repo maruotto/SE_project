e66ac5179069cc9a442e7f757834bec08d5c7839
Gianluca <Gianluca@PC-Gianluca.mshome.net>
2021-11-30 17:04:44 +0100

    add "Command" interface

---
+++ src/se_project_g9/Command.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package se_project_g9;
+
+/**
+ *
+ * @author group 9
+ */
+public interface Command {
+    void execute();
+    void undo();
+}
