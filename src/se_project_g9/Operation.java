7b105a4057814b0261dd28515a1bab6dac3bb4dd
luigi <luigi@air-di-luigi.home>
2021-12-09 18:19:25 +0100

    Adding operation class documentation

--- src/se_project_g9/Operation.java
+++ src/se_project_g9/Operation.java
@@ -21,6 +21,9 @@ public class Operation implements ApplicationOperation {
     private final Stack<Command> operationsPerformed;
     private final Interpreter i;
 
+    /**
+     * 
+     */
     public Operation() {
         this.numberStack = new NumberStack<>();
         this.variables = new Variables();      
@@ -32,26 +35,52 @@ public class Operation implements ApplicationOperation {
         
     }
 
+    /**
+     * this method returns the operations performed
+     * @return
+     */
     protected Stack<Command> getOperationsPerfomed() {
         return operationsPerformed;
     }
 
+    /**
+     * this method returns the number stack
+     * @return
+     */
     protected PersonalizedStack<ComplexNumber> getNumberStack() {
         return numberStack;
     }
 
+    /**
+     * this method returns the variables
+     * @return
+     */
     protected Variables getVariables() {
         return variables;
     }
 
+    /**
+     * this methods returns the operations
+     * @return
+     */
     protected UDAllOp getOperations() {
         return operations;
     }
 
+    /**
+     *
+     * @param name the custom operation name
+     * @param input the commands sequence
+     * @throws InputNumberException
+     */
     public void addUDOperation(String name, String input) throws InputNumberException {
         performCommand(new AddOperationCommand(operations,name,input));
     }
 
+    /**
+     * this methods allows to revoke the last operation done.
+     * @throws ImpossibleUndo
+     */
     public void undo() throws ImpossibleUndo {
         Command op = operationsPerformed.pop(); //throw empty stack exception
         try {
@@ -62,15 +91,30 @@ public class Operation implements ApplicationOperation {
 
     }
 
+    /**
+     * this method translate the input and execute the operation
+     * @param input the operation name 
+     * @throws InputNumberException
+     */
     public void performOperation(String input) throws InputNumberException {
         performCommand(i.translateInput(input, false));
     }
     
+    /**
+     * this method execute the command passed as parameter
+     * @param op the command to execute
+     * @throws InputNumberException
+     */
     public void performCommand(Command op) throws InputNumberException {
         op.execute();
         operationsPerformed.push(op);
     }
 
+    /**
+     * this method takes the stack first element and add it into variable
+     * @param variable the variable name
+     * @throws Exception
+     */
     public void addToVariable(Character variable) throws Exception {
 
         Command cm = new VInsertCommand(variables, numberStack, variable);
@@ -78,27 +122,59 @@ public class Operation implements ApplicationOperation {
 
     }
 
+    /**
+     * this method takes the variable value and push it into the stack
+     * @param variable the variable name
+     * @throws Exception
+     */
     public void pushValueOf(Character variable) throws Exception {
         performCommand(new VPushCommand(variables, variable, numberStack));
     }
 
+    /**
+     * this method takes the stack first element and sums it with the variable value
+     * @param variable the variable name
+     * @throws Exception
+     */
     public void addToValue(Character variable) throws Exception {
         performCommand(new VAddCommand(variables, variable, numberStack));
     }
 
+    /**
+     * this method takes the stack first element and subtracts it with the variable value
+     * @param variable the variable name
+     * @throws Exception
+     */
     public void subToValue(Character variable) throws Exception {
         performCommand(new VSubCommand(variables, variable, numberStack));
 
     }
 
+    /**
+     * this method remove a defined operation
+     * @param key the operation name
+     * @throws InputNumberException
+     */
     public void removeOperation(String key) throws InputNumberException {
         performCommand(new DeleteCommand(operations, key));
     }
     
+    /**
+     * this method takes in input the operation name and the new operation sequence and modifies it
+     * @param key the operation name
+     * @param newValue the new sequence of operations to do
+     * @throws InputNumberException
+     */
     public void modifyOperation(String key, String newValue) throws InputNumberException {
         performCommand(new ModifyOperationCommand(operations, key, new UDOperation(newValue)));
     }
     
+    /**
+     * this method takes in input the operation name and the new name and modifies it
+     * @param key the operation name
+     * @param newKey the new operation name
+     * @throws InputNumberException
+     */
     public void modifyOperationName(String key, String newKey) throws InputNumberException {   
         UDOperation op = new UDOperation();
         if(this.operations.containsKey(newKey))
@@ -112,26 +188,49 @@ public class Operation implements ApplicationOperation {
         performCommand(cm);
     }
     
+    /**
+     * this method executes the drop operation
+     * @throws InputNumberException
+     */
     public void drop() throws InputNumberException{
         performCommand(new DropCommand(numberStack));
     }
 
+    /**
+     * this method executes the dup operation
+     * @throws InputNumberException
+     */
     public void dup() throws InputNumberException {
         performCommand(new DupCommand(numberStack));
     }
 
+    /**
+     * this method executes the swap operation
+     * @throws InputNumberException
+     */
     public void swap() throws InputNumberException {
         performCommand(new SwapCommand(numberStack));
     }
 
+    /**
+     * this method executes the clear operation
+     * @throws InputNumberException
+     */
     public void clear() throws InputNumberException {
         performCommand(new ClearCommand(numberStack));
     }
-
+    /**
+     * this method executes the over operation
+     * @throws InputNumberException 
+     */
+    
     void over() throws InputNumberException {
         performCommand(new OverCommand(numberStack));
     }
-
+    /**
+     * this method executes the invert operation
+     * @throws InputNumberException 
+     */
     void invert() throws InputNumberException {
         performCommand(new InvertCommand(numberStack));
     }
